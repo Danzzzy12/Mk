31c164703fdbc762609b938b2e9c05f1289be3a4
Danzzzy12 <165837131+Danzzzy12@users.noreply.github.com>
2025-01-21 01:43:37 +0900

    Add files via upload

---
+++ CreateFacebook.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+AriifffAtacckermanFromeLautannnSamudraaaEaaaaaaaa
+Omongan Tinggi Ga ada Aksi Ngerasa Bang?
+"""
+from faker import Faker
+from bs4 import BeautifulSoup as bs
+import time, os, re, requests, random, sys, json, subprocess, uuid
+from random import randint as RandomRandint
+from random import choice as RandomChoice
+class create:
+	def __init__(self):
+		self.main()
+	
+	def main(self):
+		global start, gender, umail, pwx,nama, gdr, mail, pwd, cln, thn
+		os.system('clear')
+
+class create:
+	def __init__(self):
+		self.main()
+	
+	def main(self):
+		global start, gender, umail, pwx,nama, gdr, mail, pwd, cln, thn
+		os.system('clear')
+		print("\t\t\t  © CREATE FACEBOOK \n")
+		total = int(input('Creat New Idzz Berapa? : '))
+		AwmasbrewSayangArif = input('MasukanDelay: ')
+		if AwmasbrewSayangArif == '' or AwmasbrewSayangArif == ' ':
+			AwmasbrewSayangArif = 1
+		le = int(AwmasbrewSayangArif)*60
+		for i in range(total):
+			Create();self.Looping(le)
+		exit("selesai")
+	def Looping(self,le):
+		for c in range(le+1):
+			print(f"Time: {M}{c}{P} Detak | Crot: {H}{Ok}{P} | Ngising: {K}{Cp}{P}     ", end='\r');sys.stdout.flush()
+			le-=1
+			time.sleep(1)
+
+def main():
+    directories = ['/sdcard/CreateFacebook', 'Masssbrewww'] 
+    create_directories(directories)
+    try:
+        Clear()
+        create().main()
+    except requests.exceptions.ConnectionError:
+        print('Connection closed.')
+    except Exception as e:
+        print(f'Unexpected error occurred: {e}')
+        
+if __name__=="__main__":
+	create()
